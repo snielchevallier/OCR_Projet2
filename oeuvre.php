90c7955566d2958e63a1fa48cdc30f8fe40f87df
snielchevallier <snielchevallier@gmail.com>
2026-02-09 11:11:03 +0100

    oeuvre.php - détail de l'oeuvre récupéré à partir de la bdd

--- oeuvre.php
+++ oeuvre.php
@@ -1,6 +1,6 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+    require $_SERVER['DOCUMENT_ROOT'] . '\config\bdd.php';
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
@@ -10,16 +10,28 @@
     $oeuvre = null;
 
     // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
+    /*foreach($oeuvres as $o) {
         // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
         if($o['id'] === intval($_GET['id'])) {
             $oeuvre = $o;
             break; // On stoppe le foreach si on a trouvé l'oeuvre
         }
     }
+    */
+    $conn = connexion();
+    if ($conn) {
+        try {
+            $stmt = $conn->prepare("SELECT * FROM oeuvres WHERE id = :id");
+            $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+            $stmt->execute();
+            $oeuvre = $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    if(!$oeuvre) {
         header('Location: index.php');
     }
 ?>
